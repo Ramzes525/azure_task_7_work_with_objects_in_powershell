57415749642b2d9ec3f6d8490ee932173b48835f
Gariman <garimanblack1@gmail.com>
2025-02-10 20:31:08 +0200

    Solution-Object++

--- task.ps1
+++ task.ps1
@@ -1,1 +1,12 @@
-# Write your code here
+$repofile = Get-ChildItem -Path "./data/*.json"
+$sizevm = "Standard_B2pts_v2"
+$resultfile = @()
+
+foreach ($file in $repofile) {
+    $jsondata = Get-Content -Path $file.FullName | ConvertFrom-json
+    if ($jsondata | Where-Object {$_.Name -eq $sizevm}) {
+        $region = $file.BaseName
+        $resultfile += $region
+    }
+}
+ $resultfile | ConvertTo-Json | Out-File -Path ./result.json
